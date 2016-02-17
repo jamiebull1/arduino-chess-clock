79178eedef3ac1d0fa79eee0970dd6401ed5ee86
Jamie Bull <jamie.bull@oco-carbon.com>
2016-02-17 09:38:41 +0100

    Change button #defines to enum

--- arduino-chess-clock.ino
+++ arduino-chess-clock.ino
@@ -30,12 +30,17 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 // define some values used by the panel and buttons
 int lcd_key     = 0;
 int adc_key_in  = 0;
-#define btnRIGHT  0
-#define btnUP     1
-#define btnDOWN   2
-#define btnLEFT   3
-#define btnSELECT 4
-#define btnNONE   5
+
+enum Button 
+{
+    btnRIGHT,
+    btnUP,
+    btnDOWN,
+    btnLEFT,
+    btnSELECT,
+    btnNONE
+};
+
 // read the buttons
 int read_LCD_buttons()
 {
