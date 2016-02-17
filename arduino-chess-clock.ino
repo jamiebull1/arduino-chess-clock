eb14dfe4ccf495e7b813762fd61e674a7a4a91f5
Jamie Bull <jamie.bull@oco-carbon.com>
2016-02-17 17:43:32 +0100

    Reorder functions to have setup and loop nearer the top

--- arduino-chess-clock.ino
+++ arduino-chess-clock.ino
@@ -28,7 +28,7 @@ Jamie Bull, February 2016
 // select the pins used on the LCD panel
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 // define some values used by the panel and buttons
-int lcdKey     = 0;
+int lcdKey      = 0;
 int adc_key_in  = 0;
 
 enum Button 
@@ -60,7 +60,6 @@ int read_LCD_buttons()
 }
 //================================================================
 
-// Code below here is mine
 // Initialise some global parameters
 bool isUsingMenu = false;  // whether we are in the time-setting menu
 bool isGameOver = false;
@@ -97,6 +96,156 @@ class Player
         }
 };
 
+// initialise the players
+Player p0 = {0, "",               "  --  ", 0};  // dummy player, active when player clocks are not counting down
+Player p1 = {5, "Player1 mins: ", "  <-  ", 0};
+Player p2 = {5, "Player2 mins: ", "  ->  ", 0};
+
+// set a pointer to the first player to be shown in the time-setting menu
+Player* menuPlayer = &p1;
+// set the active player pointer to the player to start with
+Player* activePlayer = &p0;
+
+void setup()
+{
+    lcd.begin(16, 2);  
+}
+
+// Main loop
+void loop()
+{
+    activePlayer->UpdateCounter(p0, p1, p2);;  // do this every loop
+    lcdKey = read_LCD_buttons();  
+    if (isUsingMenu) 
+    {
+        updateMenuScreen();
+    } else {
+        updateTimerScreen();
+    }
+}
+
+void resetGame()
+// reset game to starting state
+{
+    p0.secondsRun += (p1.secondsRun + p2.secondsRun);
+    p1.secondsRun = 0;
+    p2.secondsRun = 0;
+    activePlayer = &p0;
+    isGameOver = false;
+}
+
+void updateTimerScreen()
+{
+    switch (lcdKey)               
+    {
+    case btnLEFT: { activePlayer = &p1; break; }  // set player 1 active
+    
+    case btnRIGHT: { activePlayer = &p2; break; } // set player 2 active
+    
+    case btnUP: { activePlayer = &p0; break; }    // pause the timers
+    
+    case btnDOWN: { break; }                      // not used
+    
+    case btnSELECT: // to activate menu (or reset if game is over)
+        {
+        if (isGameOver)
+        {
+            resetGame();
+        } else {
+            // activate the menu
+            isUsingMenu = true;   
+            activePlayer = &p0;
+        }
+        delay(500);  // delay required otherwise the button fires repeatedly
+        break;
+        }
+    case btnNONE:  // update timer if no button pressed
+        {
+        if (isGameOver) {
+            // do nothing and wait for btnSELECT to reset the game
+        } else {
+            // check if either player is out of time
+            int loser = getLoserNumber();
+            
+            lcd.setCursor(0,0);
+            if (loser > 0) {
+                lcd.print("   Game Over!   ");
+                lcd.setCursor(0,1);
+                lcd.print(" Player " + String(loser) + " loses ");
+                isGameOver = true;
+            } else {
+                // update timer display
+                lcd.print("P1 ChessClock P2");
+                lcd.setCursor(0,1);
+                lcd.print(timeString(p1.SecondsRemaining()));      
+                lcd.setCursor(5,1);            
+                lcd.print(activePlayer->playIndicator);        
+                lcd.setCursor(11,1);
+                lcd.print(timeString(p2.SecondsRemaining()));
+            }
+        }
+        }
+    }
+}
+
+void updateMenuScreen()
+{
+    switch (lcdKey)                
+    {
+    case btnUP: 
+        { menuPlayer->IncrementMinutes(); break; } // increase minutes
+    case btnDOWN:
+        { menuPlayer->DecrementMinutes(); break; } // decrease minutes
+    case btnLEFT:
+        {
+        // set pointer to player 1 as player to change time for
+        menuPlayer = &p1;
+        break;
+        }  
+    case btnRIGHT:
+        {
+        // set pointer to player 2 as player to change time for
+        menuPlayer = &p2;
+        break;
+        }  
+    case btnSELECT:
+        {
+        // return to timer screen
+        isUsingMenu = false;
+        delay(500);  // delay required otherwise the button fires repeatedly
+        break;       
+        }  
+    case btnNONE:
+        {
+        // update menu display
+        lcd.setCursor(0,0);
+        lcd.print("      MENU      ");
+        lcd.setCursor(0,1);
+        String playerMins;
+        if (menuPlayer->minutes < 10) {
+            playerMins = " " + String(menuPlayer->minutes);
+        } else {
+            playerMins = String(menuPlayer->minutes);
+        }
+        lcd.print(menuPlayer->menuText + playerMins);
+        break;       
+        }
+    }
+}
+    
+int getLoserNumber()
+// return the number of the losing player, or 0 if both have time remaining
+{
+    if (p1.SecondsRemaining() <= 0)
+        {
+        return 1;
+    } else if (p2.SecondsRemaining() <= 0)
+        {
+        return 2;
+    }
+    return 0;
+}
+
 String timeString(long seconds)
 // Convert seconds to an MM:SS display.
 {
@@ -116,141 +265,4 @@ String pad(int toPad)
     }
 };
 
-// initialise the players
-Player p0 = {0, "", "  --  ", 0};  // dummy player, active when player clocks are not counting down
-Player p1 = {5, "Player1 mins: ", "  <-  ", 0};
-Player p2 = {5, "Player2 mins: ", "  ->  ", 0};
 
-// set a pointer to the first player to be shown in the time-setting menu
-Player* menuPlayer = &p1;
-// set the active player pointer to the player to start with
-Player* activePlayer = &p0;
-
-void resetGame()
-// reset game to starting state
-{
-    p0.secondsRun += (p1.secondsRun + p2.secondsRun);
-    p1.secondsRun = 0;
-    p2.secondsRun = 0;
-    activePlayer = &p0;
-    isGameOver = false;
-}
-
-void setup()
-{
-    lcd.begin(16, 2);  
-    lcd.setCursor(0,0);
-    lcd.print("P1 ChessClock P2");
-}
-
-// Main loop
-void loop()
-{
-   activePlayer->UpdateCounter(p0, p1, p2);;  // do this every loop
-    lcdKey = read_LCD_buttons();  
-    if (isUsingMenu) {
-        switch (lcdKey)                
-        {
-        case btnUP: 
-            { menuPlayer->IncrementMinutes(); break; } // increase minutes
-        case btnDOWN:
-            { menuPlayer->DecrementMinutes(); break; } // decrease minutes
-        case btnLEFT:
-            {
-            // set pointer to player 1 as player to change time for
-            menuPlayer = &p1;
-            break;
-            }  
-        case btnRIGHT:
-            {
-            // set pointer to player 2 as player to change time for
-            menuPlayer = &p2;
-            break;
-            }  
-        case btnSELECT:
-            {
-            // return to timer screen
-            isUsingMenu = false;
-            delay(500);  // delay required otherwise the button fires repeatedly
-            break;       
-            }  
-        case btnNONE:
-            {
-            // update menu display
-            lcd.setCursor(0,0);
-            lcd.print("      MENU      ");
-            lcd.setCursor(0,1);
-            String playerMins;
-            if (menuPlayer->minutes < 10) {
-                playerMins = " " + String(menuPlayer->minutes);
-            } else {
-                playerMins = String(menuPlayer->minutes);
-            }
-            lcd.print(menuPlayer->menuText + playerMins);
-            break;       
-            }
-        }
-    } else {
-        switch (lcdKey)               
-        {
-        case btnLEFT: { activePlayer = &p1; break; } // player 1
-        
-        case btnRIGHT: { activePlayer = &p2; break; } // player 2
-        
-        case btnUP: { activePlayer = &p0; break; } // pause the timers
-        
-        case btnDOWN: { break; } // not used
-        
-        case btnSELECT: // activate menu (or reset if game is over)
-            {
-            if (isGameOver)
-            {
-                resetGame();
-            } else {
-                // activate the menu
-                isUsingMenu = true;   
-                activePlayer = &p0;
-            }
-            delay(500);  // delay required otherwise the button fires repeatedly
-            break;
-            }
-        case btnNONE:  // update each loop
-            {
-            if (isGameOver) {
-                // do nothing and wait for btnSELECT to reset the game
-                break;  
-            } else {
-                // check if either player is out of time
-                if (p1.SecondsRemaining() <= 0) 
-                {  // player 1 lost
-                    lcd.setCursor(0,0);
-                    lcd.print("   Game Over!   ");
-                    lcd.setCursor(0,1);
-                    lcd.print(" Player 1 loses ");
-                    isGameOver = true;
-                    break;
-                } else if (p2.SecondsRemaining() <= 0) 
-                {  // player 2 lost
-                    lcd.setCursor(0,0);
-                    lcd.print("   Game Over!   ");
-                    lcd.setCursor(0,1);
-                    lcd.print(" Player 2 loses ");
-                    isGameOver = true;
-                    break;
-                } else {
-                    // update timer display
-                    lcd.setCursor(0,0);
-                    lcd.print("P1 ChessClock P2");
-                    lcd.setCursor(0,1);
-                    lcd.print(timeString(p1.SecondsRemaining()));      
-                    lcd.setCursor(5,1);            
-                    lcd.print(activePlayer->playIndicator);        
-                    lcd.setCursor(11,1);
-                    lcd.print(timeString(p2.SecondsRemaining()));
-                }
-                break;
-            }       
-        }
-        }
-    }
-}
